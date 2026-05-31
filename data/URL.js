7483f0de748df0b0e0ed4fce0ec690fef2b35a3d
Tamir Nusuyev <tamir140195@gmail.com>
2026-05-31 14:38:12 +0300

    fixing template literal in URLS

--- data/URL.js
+++ data/URL.js
@@ -1,9 +1,9 @@
-export const BASE_URL = 'https://www.saucedemo.com/';
+export const BASE_URL = "https://www.saucedemo.com/";
 
 export const URLS = {
-    INVENTORY: `${BASE_URL}inventory.html`,
-    CART: `${BASE_URL}cart.html`,
-    CHECKOUT_STEP_ONE: `${BASE_URL}checkout-step-one.html`,
-    CHECKOUT_STEP_TWO: `${BASE_URL}checkout-step-two.html`,
-    CHECKOUT_COMPLETE: `{BASE_URL}checkout-complete.html`,
+  INVENTORY: `${BASE_URL}inventory.html`,
+  CART: `${BASE_URL}cart.html`,
+  CHECKOUT_STEP_ONE: `${BASE_URL}checkout-step-one.html`,
+  CHECKOUT_STEP_TWO: `${BASE_URL}checkout-step-two.html`,
+  CHECKOUT_COMPLETE: `${BASE_URL}checkout-complete.html`,
 };
